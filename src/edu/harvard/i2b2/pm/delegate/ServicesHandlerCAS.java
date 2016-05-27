de38a659220d66d2900aed75532ef0d777ded051
Andrew Post <arpost@emory.edu>
2016-05-27 09:24:44 -0400

    Removed stale reference to the fail exception.

--- src/edu/harvard/i2b2/pm/delegate/ServicesHandlerCAS.java
+++ src/edu/harvard/i2b2/pm/delegate/ServicesHandlerCAS.java
@@ -112,8 +112,6 @@ public class ServicesHandlerCAS extends ServicesHandler {
 		    log.error("Unexpected response from CAS: " + response);
 		    throw new Exception("EINTERNAL");
 		}
-		
-                throw fail;
 	    }
 
             log.debug("CAS authenticated user:" + username);
