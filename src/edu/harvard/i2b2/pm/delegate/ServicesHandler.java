036d6ee246b93aa5a06db29a6565d349e0e20c71
nityananda reddy <nityananda.chappidi@emory.edu>
2018-04-18 12:19:41 -0400

    method change

--- src/edu/harvard/i2b2/pm/delegate/ServicesHandler.java
+++ src/edu/harvard/i2b2/pm/delegate/ServicesHandler.java
@@ -363,7 +363,7 @@ public class ServicesHandler extends RequestHandler {
 			if (password.startsWith("SessionKey:"))
 			{
 				System.out.println("insode session key");
-				getUsername();
+				//getUsername();
 				String sessionKey=password.replace("SessionKey:", "");
 				log.debug("Encrypted Session key: "+sessionKey+" passed in for validation.");
 				if (rmt.getPassword().getTokenMsTimeout() == null)
@@ -407,7 +407,7 @@ public class ServicesHandler extends RequestHandler {
 				try {
 					log.debug("Validating user: " + rmt.getUsername());
 					System.out.println("Validating user: " + rmt.getUsername());
-					getUsername();
+					//getUsername();
 					UserType user = validateSuppliedPassword( rmt.getUsername(), rmt.getPassword().getValue(), params);
 					uType.setFullName(user.getFullName());
 					uType.setIsAdmin(user.isIsAdmin());
@@ -2123,8 +2123,9 @@ public class ServicesHandler extends RequestHandler {
 	}
 
 
-	private void getUsername()
+	/*private String getUsername()
 	{
+		String username = null;
 		try{
 		System.out.println("Inside user name");
 		//stub._getServiceContext().getCurrentOperationContext().getMessageContext("In");
@@ -2161,7 +2162,7 @@ public class ServicesHandler extends RequestHandler {
 		    String response = builder.toString();
 		    System.out.println("+++++response+++:"+response);
 		    int start = response.indexOf("<cas:authenticationSuccess");
-		    String username;
+		    
 		    if (start > -1) {
 		    	start = response.indexOf(">", start);
 		    	if (start < 0) {
@@ -2214,7 +2215,8 @@ public class ServicesHandler extends RequestHandler {
 		{
 			e.printStackTrace();
 		}
-	}
+		return username;
+	}*/
 	
 
 
