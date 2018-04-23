da769929dbf7b2abebc6a65aeb745168b51f936f
Andrew Post <arpost@emory.edu>
2018-04-23 18:20:01 -0400

    Remove the print statements.

--- src/edu/harvard/i2b2/pm/delegate/ServicesHandler.java
+++ src/edu/harvard/i2b2/pm/delegate/ServicesHandler.java
@@ -7,12 +7,10 @@
  * the terms of the Healthcare Disclaimer.
  ******************************************************************************/
 /*
-
  * 
  * Contributors:
  * 		Lori Phillips
  */
-
 package edu.harvard.i2b2.pm.delegate;
 
 import java.io.BufferedReader;
@@ -270,7 +268,7 @@ public class ServicesHandler extends RequestHandler {
 		PMDbDao pmDb = new PMDbDao();
 
 		log.debug("I am in the RequestHandler");
-		System.out.println("I am in the RequestHandler");
+
 		ConfigureType cType = new ConfigureType();
 		UserType uType = new UserType();
 		CellDatasType aType = new CellDatasType();
@@ -279,11 +277,10 @@ public class ServicesHandler extends RequestHandler {
 
 		try {
 			String domainId = null;
-			
-			SecurityType rmt	 = getServicesMsg.getRequestMessageType().getMessageHeader().getSecurity();
+			SecurityType rmt = getServicesMsg.getRequestMessageType().getMessageHeader().getSecurity();
 			String project = getServicesMsg.getRequestMessageType().getMessageHeader().getProjectId();
 			log.debug("My username: " + rmt.getUsername());
-			System.out.println("My username: " + rmt.getUsername());
+
 
 			String SQL_QUERY = null;
 
@@ -302,9 +299,8 @@ public class ServicesHandler extends RequestHandler {
 				throw new Exception ("Database error in getting environment data");
 			}
 			log.debug("Start parsing environment results of: " + response);
-			System.out.println("Start parsing environment results of: " + response);
+
 			Iterator it = response.iterator();
-			System.out.println("it"+it);
 			while (it.hasNext())
 			{
 				cType =(ConfigureType)it.next();
@@ -324,7 +320,6 @@ public class ServicesHandler extends RequestHandler {
 
 			//Determine authentication method
 			log.debug("Get authentication method by using domain: " + domainId);
-			System.out.println("Get authentication method by using domain: " + domainId);
 			//String method = "", domainController= "", domain= "";
 			String method = null;
 			for( it=pmDb.getEnvironmentData(domainId).iterator();it.hasNext();){
@@ -357,13 +352,11 @@ public class ServicesHandler extends RequestHandler {
 			}	
 
 			String password = rmt.getPassword().getValue();
-			log.debug("++REQUEST INFO++"+getServicesMsg.getRequestType());
-			System.out.println("++REQUEST INFO++"+getServicesMsg.getRequestType());
+
+
 			//If password begins with "SessionKey:" its a session key and decrypt it and validate it
 			if (password.startsWith("SessionKey:"))
 			{
-				System.out.println("insode session key");
-				//getUsername();
 				String sessionKey=password.replace("SessionKey:", "");
 				log.debug("Encrypted Session key: "+sessionKey+" passed in for validation.");
 				if (rmt.getPassword().getTokenMsTimeout() == null)
@@ -406,8 +399,7 @@ public class ServicesHandler extends RequestHandler {
 				//get the user from the service
 				try {
 					log.debug("Validating user: " + rmt.getUsername());
-					System.out.println("Validating user: " + rmt.getUsername());
-					//getUsername();
+
 					UserType user = validateSuppliedPassword( rmt.getUsername(), rmt.getPassword().getValue(), params);
 					uType.setFullName(user.getFullName());
 					uType.setIsAdmin(user.isIsAdmin());
@@ -2221,4 +2213,3 @@ public class ServicesHandler extends RequestHandler {
 
 
 }
-
